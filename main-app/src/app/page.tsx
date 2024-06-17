a63c6b45f095cb1ecc0b8784d5494c3e980323fc
Ivan Bogdanov <ivan@bogdanov.com>
2024-06-17 19:08:10 +0300

    renaned classes

--- main-app/src/app/page.tsx
+++ main-app/src/app/page.tsx
@@ -4,58 +4,60 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <>
-    <header>
+    <header className="header">
         <Image 
         src="/images/logo_company.svg" 
         alt="logo"
         width={178}
         height={42}
         priority/>
-      <nav>
-        <ul>Home</ul>
-        <ul>About</ul>
-        <ul>Services</ul>
-        <ul>Pages</ul>
-        <ul>Contact</ul>
-      </nav>
-            
-      <button className={`${styles.button_head} button_primary`}>Get a free quote</button>
-      
+      <nav className="header__nav">
+        <ul className="header__nav_item">Home</ul>
+        <ul className="header__nav_item">About</ul>
+        <ul className="header__nav_item">Services</ul>
+        <ul className="header__nav_item">Pages</ul>
+        <ul className="header__nav_item">Contact</ul>
+      </nav>            
+      <button className={`button button_primary ${styles.header__button}`}>Get a free quote</button>      
     </header>
     <main className={styles.main}>
-      <section id="home">
-        <div className={styles.home_top}>
-          <div className={styles.home_frame}>
-          <h1 className={styles.text_header}>Quality cleaning for your home</h1>
-          <p className={styles.text_main}>Condimentum mauris sit cursus amet id non neque pharetra nulla ornare sed facilisis senectus dapibus nibh ultrices eget suscipit aliquet et nulla magna lacus penatibus.</p>
-          <div className={styles.home_call}>
-            <button className={`${styles.button_home} button_primary`}>Get a free quote</button>
-            <Image
-            src="/images/small_phone.svg"
-            alt="sp"
-            width={72}
-            height={72}/>
-            <div>
-              <p>Call us now</p>
-              <h1>(414) 567 - 2109</h1>
+      <section className="main__section" id="home">
+        <div className={styles.main__home_top}>
+          <div className={styles.main__home_frame}>
+            <h1 className={styles.main__home_text_header}>Quality cleaning for your home</h1>
+            <p className={styles.main__home_text}>Condimentum mauris sit cursus amet id non neque pharetra nulla ornare sed facilisis senectus dapibus nibh ultrices eget suscipit aliquet et nulla magna lacus penatibus.</p>
+            <div className={styles.main__home_call}>
+              <button className={`button button_primary ${styles.home__button}`}>Get a free quote</button>
+              <Image
+              className={styles.main__home_call_img}
+              src="/images/small_phone.svg"
+              alt="sp"
+              width={72}
+              height={72}/>
+              <div>
+                <p>Call us now</p>
+                <h4>(414) 567 - 2109</h4>
+              </div>
             </div>
           </div>
-          </div>
-        <Image
-        src="/images/big_cleaner_woman_top.svg"
-        alt="bcwt"
-        width={720}
-        height={734}
-        priority/>
+          <Image
+          className={styles.main__home_top_img}
+          src="/images/big_cleaner_woman_top.svg"
+          alt="bcwt"
+          width={720}
+          height={734}
+          priority/>
         </div>
-        <div className={styles.home_bottom}>
-
+        <div className={styles.main__home_bottom}>
+          <h2>How our service works?</h2>
+          <p>Sagittis nibh scelerisque vitae eget vulputate sem elementum sed neque nisi felis non ultrices massa id egestas quam velit pretium nu.</p>
+          <div className={styles.main__home_bottom_steps}></div>
         </div>
       </section>      
-      <section id="about"></section>
-      <section id="services"></section>
-      <section id="pages"></section>
-      <section id="contact"></section>
+      <section className="main__section" id="about"></section>
+      <section className="main__section" id="services"></section>
+      <section className="main__section" id="pages"></section>
+      <section className="main__section" id="contact"></section>
     </main>
     <footer></footer>
     </>
