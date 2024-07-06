582e2a8d89cab0c26b1941a73bd30103d72db338
Ivan Bogdanov <ivan@bogdanov.com>
2024-07-06 15:51:38 +0300

    fix

--- main-app/next.config.js
+++ main-app/next.config.js
@@ -1,19 +1,3 @@
-// import { PHASE_PRODUCTION_BUILD } from 'next/constants';
-
-// /** @type {import('next').NextConfig} */
-
-// const nextConfig = (phase) => {
-//     if (phase === PHASE_PRODUCTION_BUILD) {        
-//         return {
-//             output: 'export',
-//             trailingSlash: true,
-//             basePath: "/cleaning",
-//         }
-//     }    
-// }
-
-// export default nextConfig;
-
 const { PHASE_PRODUCTION_BUILD } = require('next/constants')
  
 module.exports = (phase, { defaultConfig }) => {
